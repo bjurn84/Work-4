b96be8838d88eaedf3f080996ccf6a1dfa96c887
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2022-11-12 17:09:18 +0400

    Задача 4

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите целые числа через пробел: ");
+string data = Console.ReadLine();
+
+string[] dataList = data.Split(" ");
+Console.WriteLine(data);
+int[] intList = new int[dataList.Length];
+
+for (int i=0; i<dataList.Length; i += 1){
+    intList [i] = (Convert.ToInt32(dataList[i]));
+    Console.WriteLine(dataList[i]);
+}
+int max = intList[0];
+for (int i=0; i<dataList.Length; i +=1)
+    if (max < intList[i]) max = intList[i];
+
+
+Console.WriteLine ("Максимальное число из {0} будет - {max}");
+Console.WriteLine ("Максимальное число из {0} будет - {1}", data, max);
+Console.WriteLine ("Максимальное число из " + data + " будет - " + max);
